d07b9ee24a624c07cbed4b3d62dbb49268be2a5f
Donny's Macbook <donnywals@gmail.com>
2012-05-29 11:20:44 +0200

    Deeltoets 3 *af gemaakt* (voldaan aan criteria)

--- deeltoets3/js/main.js
+++ deeltoets3/js/main.js
@@ -1,19 +1,46 @@
+var originalNavHtml;
+
 $(function() {
+	originalNavHtml = $("nav").html();
+	
+	init();
 	if($("html").hasClass("touch")) {
 		$(".touch a").addClass("main-color");
 	}
-	
-	if($(window).width()<501 /*&& $("html").hasClass("touch")*/) {
-	// DO HTML REPLACEMENT HERE (TAKE NAV AND PUT IT BACK WITH WRAPPER
-		$("nav").prepend("<div id=\"nav-holder\">");
-		$("nav ul").append("</div>");
-		$("#nav-holder").css({
-			"height": 0,
-			"overflow": "hidden"
+	$(window).resize(function() { init (); });
+});
+
+
+function init () {
+	if($(window).width()<570 && $("html").hasClass("touch")) {
+		var navhtml = originalNavHtml;
+		navhtml = "<div id=\"nav-holder\">"+navhtml+"</div>";
+		$("nav").html(navhtml);
+		
+		$("#nav-holder").css("height", 0);
+		$("#nav-holder").css("overflow", "hidden");
+		
+		$("nav").prepend("<span id=\"nav-link\">Navigatie</span>");
+		$("#nav-link").css({
+			"padding": "1em",
+			"margin": "1em",
+			"border-radius": "0.5em",
+			"background": "#ddd",
+			"border": "1px solid #bbb",
+			"display": "block",
+			"text-align": "center"
 		});
-		$("nav").prepend("<a href=\"#\" id=\"nav-link\">Navigatie</a>");
-		$("#nav-link").click(function(){
+		
+		$("#nav-link").on("touchend", function(){
+			var navHeight = parseFloat($("#nav-holder").css("height"));
+			if(navHeight > 0) {
+				$("#nav-holder").css("height", 0);
+			} else {
+				$("#nav-holder").css("height", "auto");
+			}
 			
 		});
+	} else {
+		$("nav").html(originalNavHtml);
 	}
-});
+}
